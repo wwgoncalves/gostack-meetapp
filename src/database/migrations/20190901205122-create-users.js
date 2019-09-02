af686bbb727ed0fe9760059b3b910de5866acf78
lilo <williamcco@gmail.com>
2019-09-02 19:44:06 -0300

    fix: postgresql correct uuid generation via sequelize migrations

--- src/database/migrations/20190901205122-create-users.js
+++ src/database/migrations/20190901205122-create-users.js
@@ -1,36 +1,38 @@
-const uuid = require("uuid/v4");
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("users", {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: () => uuid(),
-        allowNull: false,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password_hash: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    });
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+      .then(() => {
+        return queryInterface.createTable("users", {
+          id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.literal("uuid_generate_v4()"),
+            allowNull: false,
+            primaryKey: true,
+          },
+          name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+          },
+          password_hash: {
+            type: Sequelize.STRING,
+            allowNull: false,
+          },
+          created_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+          updated_at: {
+            type: Sequelize.DATE,
+            allowNull: false,
+          },
+        });
+      });
   },
 
   down: queryInterface => {
