dd3ff875803f2642d15645ed4a42c752a3b7f64c
lilo <williamcco@gmail.com>
2019-09-16 02:18:03 -0300

    file uploading and serving

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import FileController from "./app/controllers/FileController";
 
 import authMiddleware from "./app/middlewares/auth";
+import multerConfig from "./config/multer";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
@@ -24,6 +28,6 @@ routes.put("/users", UserController.update);
 // routes.post("/subscriptions", SubscriptionController.store);
 // routes.get("/subscriptions", SubscriptionController.index);
 
-// routes.post("/files", --multer--, FileController.store);
+routes.post("/files", upload.single("file"), FileController.store);
 
 export default routes;
