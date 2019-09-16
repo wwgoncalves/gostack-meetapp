e91537b10eb2a00c8c65d8097a83b57c3921b6b7
lilo <williamcco@gmail.com>
2019-09-15 23:08:03 -0300

    routes and controllers to implement

--- src/routes.js
+++ src/routes.js
@@ -14,4 +14,16 @@ routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
 
+// routes.post("/meetups", MeetupController.store);
+// routes.get("/meetups", MeetupController.index);
+// routes.put("/meetups/:id", MeetupController.update);
+// routes.delete("/meetups/:id", MeetupController.delete);
+
+// routes.get("/arrangements", ArrangementController.index);
+
+// routes.post("/subscriptions", SubscriptionController.store);
+// routes.get("/subscriptions", SubscriptionController.index);
+
+// routes.post("/files", --multer--, FileController.store);
+
 export default routes;
